2d0b0b86a623f91de81312dfce05fe1af5c458c4
tusharkhavale <tushar2717@gmail.com>
2018-03-19 19:24:36 +0530

    First Commit

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Wrld;
+
+public class MainController : MonoBehaviour {
+
+	private static MainController instance;
+	public WrldMap wrldMap;
+	public UIController uiController;
+
+
+	/// <summary>
+	/// Awake this instance.
+	/// </summary>
+	void Awake()
+	{
+		instance = this;
+	}
+
+	/// <summary>
+	/// Gets the instance.
+	/// </summary>
+	/// <returns>The instance.</returns>
+	public static MainController GetInstance()
+	{
+		if (instance != null) 
+		{
+			return instance;
+		}
+		return null;
+	}
+
+	/// <summary>
+	/// Loads the map.
+	/// </summary>
+	/// <param name="latitude">Latitude.</param>
+	/// <param name="longitude">Longitude.</param>
+	public void LoadMap(double latitude, double longitude)
+	{
+		wrldMap.m_latitudeDegrees = latitude;
+		wrldMap.m_longitudeDegrees = longitude;
+		wrldMap.gameObject.SetActive (true);
+	}
+
+
+	public void SetCurrentLocation(float latitude, float longitude)
+	{
+		uiController.SetCurrentLocation (latitude, longitude);
+	}
+
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	private MainController mainController;
+
+	private Text txtCurrLat;
+	private Text txtCurrLong;
+
+	private InputField inputLat;
+	private InputField inputLong;
+
+	private Button btnLoad;
+
+	private double latitude;
+	private double longitude;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start()
+	{
+		LoadReferences ();
+		AddButtonListners ();
+	}
+
+
+	/// <summary>
+	/// Loads the references.
+	/// </summary>
+	private void LoadReferences()
+	{
+		mainController = MainController.GetInstance ();
+		Transform startPage = transform.Find ("StartPage");
+		txtCurrLat = startPage.Find ("CurrentLocation").Find ("txtLatValues").GetComponent<Text> ();
+		txtCurrLong = startPage.Find ("CurrentLocation").Find ("txtLongValues").GetComponent<Text> ();
+
+		inputLat = startPage.Find ("GoTo").Find ("inputLatitude").GetComponent<InputField> (); 
+		inputLong = startPage.Find ("GoTo").Find ("inputLongitude").GetComponent<InputField> (); 
+
+		btnLoad = startPage.Find ("GoTo").Find ("btnGo").GetComponent<Button> ();
+	}
+
+	/// <summary>
+	/// Adds the button listners.
+	/// </summary>
+	private void AddButtonListners()
+	{
+		btnLoad.onClick.AddListener (this.OnClickLoad);
+	}
+
+
+	/// <summary>
+	/// Raises the click load event.
+	/// </summary>
+	private void OnClickLoad()
+	{
+		latitude = double.Parse(inputLat.text);
+		longitude = double.Parse(inputLong.text);
+
+		mainController.LoadMap (latitude, longitude);
+	}
+
+	/// <summary>
+	/// Sets the current location display.
+	/// </summary>
+	/// <param name="latitude">Latitude.</param>
+	/// <param name="longitude">Longitude.</param>
+	public void SetCurrentLocation(float latitude, float longitude)
+	{
+		txtCurrLat.text =  ""+latitude;
+		txtCurrLong.text = ""+longitude;
+	}
+}
